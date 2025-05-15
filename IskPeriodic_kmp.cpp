f412064fbf1dd80e6972f87f60a5ed02ab752048
YanBatytskiy <YanBatytskiy@gmail.com>
2025-05-15 16:08:47 +0300

    first commit

--- IskPeriodic_kmp.cpp
+++ IskPeriodic_kmp.cpp
@@ -2,24 +2,35 @@
 #include <string>
 #include <vector>
 
-// Функция построения LPS массива
+/**
+ * @brief Построение массива LPS (Longest Prefix Suffix).
+ *
+ * Массив LPS показывает длину максимального префикса,
+ * который одновременно является суффиксом для подстроки,
+ * заканчивающейся на текущем символе.
+ *
+ * @param lps Вектор, куда записывается построенный LPS-массив.
+ * @param str Исходная строка для анализа.
+ */
 void buildLps(std::vector<int> &lps, const std::string &str) {
 
+  // Проверка на пустую строку и пустой вектор.
   if (lps.empty() && !str.size()) {
     std::cout << "Error. Was sent str = 0.";
     return;
   }
 
-  // Добавляем новые элементы в lps
+  // Инициализация массива LPS значениями -1
   for (int i = 0; i < str.size(); ++i)
     lps.push_back(-1);
 
-  lps[0] = 0;
+  lps[0] = 0; // Первое значение всегда 0.
 
-  // Построение индексов для новых символов
+  // Индексы для прохода по строке
   int i = 1;
   int j = 0;
 
+  // Цикл построения массива LPS
   while (i < str.size()) {
     if (str[i] == str[j]) {
       ++j;
@@ -36,7 +47,15 @@ void buildLps(std::vector<int> &lps, const std::string &str) {
   }
 }
 
-// Функция анализа строки на кратность
+/**
+ * @brief Анализирует входную строку на периодичность.
+ *
+ * Функция выводит LPS-массив, анализируемую строку,
+ * найденный минимальный повторяющийся шаблон (если есть),
+ * и выводит длину этого шаблона как K.
+ *
+ * @param str Анализируемая строка.
+ */
 void printAnalysis(const std::string &str) {
 
   std::vector<int> lps;
@@ -44,12 +63,13 @@ void printAnalysis(const std::string &str) {
 
   buildLps(lps, str);
 
+  // Вывод полученного LPS-массива
   std::cout << std::endl;
-
   for (int i = 0; i < lps.size(); ++i)
     std::cout << lps[i] << ", ";
   std::cout << std::endl;
 
+  // Анализ периодичности строки на основе LPS-массива
   int strLength = str.size();
   int maxSuffLength = strLength - lps[lps.size() - 1];
   int k = strLength % maxSuffLength;
@@ -58,6 +78,7 @@ void printAnalysis(const std::string &str) {
 
   std::string pattern = str.substr(0, maxSuffLength);
 
+  // Вывод результатов анализа
   std::cout << "Analyzing string: " << str << std::endl;
   std::cout << "Pattern string: " << pattern << std::endl;
 
@@ -72,6 +93,11 @@ void printAnalysis(const std::string &str) {
   std::cout << std::endl;
 }
 
+/**
+ * @brief Точка входа в программу.
+ *
+ * Запускает функцию анализа строк с несколькими примерами.
+ */
 int main() {
 
   printAnalysis("abcabcabcabcabcabc");
